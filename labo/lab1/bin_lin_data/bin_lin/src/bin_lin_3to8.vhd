d2f4e979342891365dd8a8016a983911f10baa5c
doeni98 <denis_bourqui@hotmail.com>
2020-09-23 11:08:29 +0200

    edited binlin3to8

--- labo/lab1/bin_lin_data/bin_lin/src/bin_lin_3to8.vhd
+++ labo/lab1/bin_lin_data/bin_lin/src/bin_lin_3to8.vhd
@@ -34,20 +34,21 @@ architecture flot_don of bin_lin_3to8 is
 
 begin
 
-    lin_o(0) <= '1' when bin_i .....   else
+    lin_o(0) <= '1' when  true  else
                 '0';
-    lin_o(1) <= '1' when .....   else
+    lin_o(1) <= '1' when bin_i > "001"  else
                 '0';
-    lin_o(2) <= '1' when  ....
-  
-    lin_o(3) <= '1' when  ....
-  
-    lin_o(4) <= '1' when  ....
-  
-    lin_o(5) <= '1' when  ....
-  
-    lin_o(6) <= '1' when  ....
-  
-    lin_o(7) <= '1' when  ....
+    lin_o(2) <= '1' when  bin_i > "010" else
+                '0';
+    lin_o(3) <= '1' when  bin_i > "011" else
+                '0';
+    lin_o(4) <= '1' when  bin_i > "100" else
+                '0';
+    lin_o(5) <= '1' when  bin_i > "101" else
+                '0';
+    lin_o(6) <= '1' when  bin_i > "110" else
+                '0';
+    lin_o(7) <= '1' when  bin_i > "111" else
+                '0';
 
 end flot_don;
